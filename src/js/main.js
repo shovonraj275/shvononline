9546ab7f7319076d09fb96fb45d821852b4141f9
Ezaz ahamed <73046376+shovonraj275@users.noreply.github.com>
2026-07-18 11:45:28 +0600

    du and  contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,89 +1,244 @@
 // <!-- ========== Start Section ========== -->
 let NEWShop = "";
-const Shop = [
-  "Select One",
-  "Bkash",
-  "Nogad",  
-  "Rocket",
-  "Upay"
-  
-];
+const Shop = ["Select One", "Bkash", "Nogad", "Rocket", "Upay"];
 Shop.forEach(myfncrtion_shop);
 document.getElementById("Shop001").innerHTML = NEWShop;
 function myfncrtion_shop(items1) {
-  NEWShop += "<option value=" + items1 +">" + items1 + "</option>";
+  NEWShop += "<option value=" + items1 + ">" + items1 + "</option>";
 }
-function hem(){
+function hem() {
   let NEWBkah = "";
   let NEWNogad = "";
-  const Bkash = [
-    "favourite Bkash Agent",
-    "Normal Bkash Agent",
-  ];
-  const Nogada = [
-    "With Nogad Apps",
-    "Without Nogad apps",
-  ];
-  let ro = `<option  value="Rocket" >Rocket</option>` ;
-  let upay = `<option  value="Upay" >Upay</option>` ;
-Bkash.forEach(myfncrtion_Bkash);
-Nogada.forEach(myfncrtion_Nogada);
-function myfncrtion_Bkash(items7,index){
-  NEWBkah += "<option value=" +"Bkash"+index +items7 + ">" + items7 + "</option>";
-}
-function myfncrtion_Nogada(items8,index){
-  NEWNogad += "<option value="+"Nogad"+index +items8+ ">" + items8 + "</option>";
-}
-  var sop_1= document.getElementById("Shop001").value
+  const Bkash = ["favourite Bkash Agent", "Normal Bkash Agent"];
+  const Nogada = ["With Nogad Apps", "Without Nogad apps"];
+  let ro = `<option  value="Rocket" >Rocket</option>`;
+  let upay = `<option  value="Upay" >Upay</option>`;
+  Bkash.forEach(myfncrtion_Bkash);
+  Nogada.forEach(myfncrtion_Nogada);
+  function myfncrtion_Bkash(items7, index) {
+    NEWBkah +=
+      "<option value=" + "Bkash" + index + items7 + ">" + items7 + "</option>";
+  }
+  function myfncrtion_Nogada(items8, index) {
+    NEWNogad +=
+      "<option value=" + "Nogad" + index + items8 + ">" + items8 + "</option>";
+  }
+  var sop_1 = document.getElementById("Shop001").value;
   console.log(sop_1);
-  if (sop_1 == "Bkash"){
-    opt =NEWBkah;
+  if (sop_1 == "Bkash") {
+    opt = NEWBkah;
+  } else if (sop_1 == "Nogad") {
+    opt = NEWNogad;
+  } else if (sop_1 == "Rocket") {
+    opt = ro;
+  } else if (sop_1 == "Upay") {
+    opt = upay;
   }
-  else if(sop_1 == "Nogad"){
-    opt= NEWNogad;
-  }
-  else if(sop_1 == "Rocket"){
-    opt= ro;
-  }
-  else if(sop_1 == "Upay"){
-    opt= upay;
-  }
-  document.getElementById("Shop003").innerHTML= opt;
+  document.getElementById("Shop003").innerHTML = opt;
 }
-function bkasss(){
+function bkasss() {
   let amounts = document.getElementById("amount");
-  let amount =parseFloat(amounts.value)
-  let methood = document.getElementById("Shop003").value
+  let amount = parseFloat(amounts.value);
+  let methood = document.getElementById("Shop003").value;
   let bkfa = 1.49;
   let bkno = 1.85;
   let ngaps = 1.199;
   let ng = 1.5;
-  let rc =1.67;
+  let rc = 1.67;
   let up = 1.4;
   console.log(amount);
   console.log(methood);
-  if(methood=="Bkash0favourite"){
-    sch = "<h3>Your account balance is : "+ amount+" Taka</h3><h3>Cash Out Charge Rate is : "+ bkfa+"%</h3>"+"<h3> Cash Out Charge is :"+ amount*(bkfa/100)+" Taka</h3><h3> Your totel amount need : "+(amount+amount*(bkfa/100))+" Taka</h3> <h3>If You have not Cash Out Charge,You enter the amount is :"+(amount-amount*(bkfa/100))+" Taka</h3>";
+  if (methood == "Bkash0favourite") {
+    sch =
+      "<h3>Your account balance is : " +
+      amount +
+      " Taka</h3><h3>Cash Out Charge Rate is : " +
+      bkfa +
+      "%</h3>" +
+      "<h3> Cash Out Charge is :" +
+      amount * (bkfa / 100) +
+      " Taka</h3><h3> Your totel amount need : " +
+      (amount + amount * (bkfa / 100)) +
+      " Taka</h3> <h3>If You have not Cash Out Charge,You enter the amount is :" +
+      (amount - amount * (bkfa / 100)) +
+      " Taka</h3>";
+  } else if (methood == "Bkash1Normal") {
+    sch =
+      "<h3>Your account balance is : " +
+      amount +
+      " Taka</h3><h3>Cash Out Charge Rate is : " +
+      bkno +
+      "%</h3>" +
+      "<h3> Cash Out Charge is :" +
+      amount * (bkno / 100) +
+      "</h3><h3> Your totel amount need : " +
+      (amount + amount * (bkno / 100)) +
+      "</h3> <h3>If You have not Cash Out Charge,You enter the amount is :" +
+      (amount - amount * (bkno / 100)) +
+      "</h3>";
+  } else if (methood == "Nogad1Without") {
+    sch =
+      "<h3>Your account balance is : " +
+      amount +
+      " Taka</h3><h3>Cash Out Charge Rate is : " +
+      ng +
+      "%</h3>" +
+      "<h3> Cash Out Charge is :" +
+      amount * (ng / 100) +
+      "</h3><h3> Your totel amount need : " +
+      (amount + amount * (ng / 100)) +
+      "</h3> <h3>If You have not Cash Out Charge,You enter the amount is :" +
+      (amount - amount * (ng / 100)) +
+      "</h3>";
+  } else if (methood == "Nogad0With") {
+    sch =
+      "<h3>Your account balance is : " +
+      amount +
+      " Taka</h3><h3>Cash Out Charge Rate is : " +
+      ngaps +
+      "%</h3>" +
+      "<h3> Cash Out Charge is :" +
+      amount * (ngaps / 100) +
+      "</h3><h3> Your totel amount need : " +
+      (amount + amount * (ngaps / 100)) +
+      "</h3> <h3>If You have not Cash Out Charge,You enter the amount is :" +
+      (amount - amount * (ngaps / 100)) +
+      "</h3>";
+  } else if (methood == "Rocket") {
+    sch =
+      "<h3>Your account balance is : " +
+      amount +
+      " Taka</h3><h3>Cash Out Charge Rate is : " +
+      rc +
+      "%</h3>" +
+      "<h3> Cash Out Charge is :" +
+      amount * (rc / 100) +
+      "</h3><h3> Your totel amount need : " +
+      (amount + amount * (rc / 100)) +
+      "</h3> <h3>If You have not Cash Out Charge,You enter the amount is :" +
+      (amount - amount * (rc / 100)) +
+      "</h3>";
+  } else if (methood == "Upay") {
+    sch =
+      "<h3>Your account balance is : " +
+      amount +
+      " Taka</h3><h3>Cash Out Charge Rate is : " +
+      up +
+      "%</h3>" +
+      "<h3> Cash Out Charge is :" +
+      amount * (up / 100) +
+      "</h3><h3> Your totel amount need : " +
+      (amount + amount * (up / 100)) +
+      "</h3> <h3>If You have not Cash Out Charge,You enter the amount is :" +
+      (amount - amount * (up / 100)) +
+      "</h3>";
+  } else {
+    sch = `<h1 class="baksss">Plase full fill correctly. thank you!</h1> ${methood}`;
   }
-  else if(methood=="Bkash1Normal" ){
-    sch = "<h3>Your account balance is : "+ amount+" Taka</h3><h3>Cash Out Charge Rate is : "+ bkno+"%</h3>"+"<h3> Cash Out Charge is :"+ amount*(bkno/100)+"</h3><h3> Your totel amount need : "+(amount+amount*(bkno/100))+"</h3> <h3>If You have not Cash Out Charge,You enter the amount is :"+(amount-amount*(bkno/100))+"</h3>";
-  }
-  else if(methood=="Nogad1Without"){
-    sch = "<h3>Your account balance is : "+ amount+" Taka</h3><h3>Cash Out Charge Rate is : "+ ng+"%</h3>"+"<h3> Cash Out Charge is :"+ amount*(ng/100)+"</h3><h3> Your totel amount need : "+(amount+amount*(ng/100))+"</h3> <h3>If You have not Cash Out Charge,You enter the amount is :"+(amount-amount*(ng/100))+"</h3>";
-  }
-  else if(methood=="Nogad0With" ){
-     sch = "<h3>Your account balance is : "+ amount+" Taka</h3><h3>Cash Out Charge Rate is : "+ ngaps+"%</h3>"+"<h3> Cash Out Charge is :"+ amount*(ngaps/100)+"</h3><h3> Your totel amount need : "+(amount+amount*(ngaps/100))+"</h3> <h3>If You have not Cash Out Charge,You enter the amount is :"+(amount-amount*(ngaps/100))+"</h3>";
-  }
-  else if(methood=="Rocket" ){
-     sch = "<h3>Your account balance is : "+ amount+" Taka</h3><h3>Cash Out Charge Rate is : "+ rc+"%</h3>"+"<h3> Cash Out Charge is :"+ amount*(rc/100)+"</h3><h3> Your totel amount need : "+(amount+amount*(rc/100))+"</h3> <h3>If You have not Cash Out Charge,You enter the amount is :"+(amount-amount*(rc/100))+"</h3>";
-  }
-  else if(methood=="Upay" ){
-     sch = "<h3>Your account balance is : "+ amount+" Taka</h3><h3>Cash Out Charge Rate is : "+ up+"%</h3>"+"<h3> Cash Out Charge is :"+ amount*(up/100)+"</h3><h3> Your totel amount need : "+(amount+amount*(up/100))+"</h3> <h3>If You have not Cash Out Charge,You enter the amount is :"+(amount-amount*(up/100))+"</h3>";
-  }
-  else{
-    sch=`<h1 class="baksss">Plase full fill correctly. thank you!</h1> ${methood}`
-  }
-  console.log(sch)
-  document.getElementById("hemout").innerHTML=sch ;
+  console.log(sch);
+  document.getElementById("hemout").innerHTML = sch;
 }
 // <!-- ========== End Section ========== -->
+
+// <!-- ========== End Section ========== -->
+let year = new Date().getFullYear();
+document.getElementById("Year").innerHTML = year;
+
+// ? ===================================== noice =========================================
+
+var schooldom = document.getElementById("school");
+var collegedom = document.getElementById("college");
+var universitydom = document.getElementById("university");
+var jobdom = document.getElementById("job");
+function school() {
+  schooldom.style.display = "block";
+  collegedom.style.display = "none";
+  universitydom.style.display = "none";
+  jobdom.style.display = "none";
+}
+function college() {
+  schooldom.style.display = "none";
+  collegedom.style.display = "block";
+  universitydom.style.display = "none";
+  jobdom.style.display = "none";
+}
+function university() {
+  schooldom.style.display = "none";
+  collegedom.style.display = "none";
+  universitydom.style.display = "block";
+  jobdom.style.display = "none";
+}
+function job() {
+  schooldom.style.display = "none";
+  collegedom.style.display = "none";
+  universitydom.style.display = "none";
+  jobdom.style.display = "block";
+}
+// ? ===================================== noice =========================================
+function du() {
+  // $("#ru_output,#cu_output").each(
+  //   function(){
+  //     $(this).css("display","none")
+  //   });
+  //  // $("#ru_output,#cu_output").hide()
+  // $("#ru_output,#cu_output").css("display", "none");
+
+  const duoutput = document.getElementById("du_output");
+  duoutput.style.display = "block";
+
+ 
+  let du_output = `
+  
+  `
+}
+const date1 = new Date('2026-08-01');
+const dateBangla = { weekday: 'long',year: 'numeric', month: 'long', day: 'numeric',hour: '2-digit', 
+    minute: '2-digit', 
+    hourCycle: 'h23'  };
+const formattedDate = date1.toLocaleDateString('bn-BD', dateBangla);
+console.log(formattedDate);
+const date2 = new Date();
+const date2BD=  date2.toLocaleDateString('bn-BD', dateBangla);
+console.log(date2BD);
+
+const dateStart = new Date('2026-07-01');
+const dateend = new Date('2026-07-20')
+const today = new Date()
+const msDifference = Math.abs(dateend - today);
+// Convert milliseconds to days
+const daysDifference = Math.floor(msDifference / (1000 * 60 * 60 * 24));
+const daysDifferencebd=daysDifference.toLocaleString("bn-bd")
+if (dateStart>today){
+  console.log("you are rady")
+}else if(dateStart<today)(
+  console.log("not rady")
+)
+// Subtracting dates returns the difference in milliseconds
+ 
+
+
+
+console.log(daysDifference); // Output: 12
+console.log(daysDifferencebd); // Output: 12
+function ru() {
+  // $("#du_output,#cu_output").css("display", "none");
+  const ruoutput = document.getElementById("ru_output");
+  ruoutput.style.display = "block";
+}
+function cu() {
+  // $("#du_output,#ru_output").css("display", "none");
+  const cu_output = document.getElementById("cu_output");
+  cu_output.style.display = "block";
+}
+
+const button = document.getElementById("toggleBtn");
+const content = document.getElementById("content");
+
+button.addEventListener("click", () => {
+  if (content.style.display === "none") {
+    content.style.display = "block"; // Shows element
+  } else {
+    content.style.display = "none"; // Hides element
+  }
+});
+
